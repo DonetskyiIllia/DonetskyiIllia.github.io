fb1ac6f7177cf1fbc6a99eb3dfd429761ac69133
DonetskyiIllia <125479114+DonetskyiIllia@users.noreply.github.com>
2023-03-26 15:37:27 +0300

    Update Pentagon.js

--- Pentagon.js
+++ Pentagon.js
@@ -1,4 +1,4 @@
-let markerVisible = { A: false, B: false, C: false, D: false, F: false};
+let markerVisible = { A: false, B: false, C: false};
 
 AFRAME.registerComponent('registerevents', {
 	init: function () {
