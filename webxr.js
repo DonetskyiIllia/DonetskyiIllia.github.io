e5e97294ef9f3291eb8510038152a77648fb66dd
DonetskyiIllia <125479114+DonetskyiIllia@users.noreply.github.com>
2023-05-21 00:16:19 +0300

    Update webxr.js

--- webxr.js
+++ webxr.js
@@ -1,56 +1,68 @@
-import * as THREE from 'three';
-
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { ARButton } from 'three/addons/webxr/ARButton.js'; //2
 
+import * as THREE from "https://unpkg.com/three@0.152.2/build/three.module.js";
 
-document.addEventListener("DOMContentLoaded", async () => {
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0xeeeeee, 1);
 
-	const scene = new THREE.Scene();
+document.body.appendChild( renderer.domElement );
+	
+const texture = new THREE.TextureLoader().load("/Textures/perlin-512.png"); 
+const materialTexture = new THREE.MeshBasicMaterial( { map:texture } );
 
-	const camera = new THREE.PerspectiveCamera();
+const texture1 = new THREE.TextureLoader().load("/Textures/grass texture.jpg"); 
+const materialTexture1 = new THREE.MeshBasicMaterial( { map:texture1 } );
+	
+const geometry = new THREE.SphereGeometry(2, 64, 32); 
+const sphere = new THREE.Mesh( geometry, materialTexture );
+sphere.position.set(0, 0, 0);
 
-	const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio);
-	document.body.appendChild(renderer.domElement);
+const geometryPlane = new THREE.PlaneGeometry( 40, 5 );
+const materialPlane = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometryPlane, materialTexture1 );
+plane.rotation.x = -45*Math.PI/180;
+plane.position.set(0, -1, 0);
+	
+let group = new THREE.Group();
+group.add(plane);
+group.add(sphere);
+scene.add( group );
 
-	const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
-	const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-	const mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(0, 0, -0.3);
-	scene.add(mesh);
+group.rotation.set(0, 0, -Math.PI/6);
 
-	var lightOne=new THREE.AmbientLight(0xffffff, 0.5);
-	scene.add(lightOne);
+camera.position.set(0, 0, 10);
 
-	const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-	scene.add(light);
+let x, y, t = 0, r = 1;
 
-	const loaderGLTF = new GLTFLoader();
+let timer=new THREE.Clock();
 
-	loaderGLTF.load( '../assets/police_hat.glb', function ( hat ) {
-		scene.add(hat.scene);
-		hat.scene.scale.set(0.075, 0.075, 0.075);
-		hat.scene.rotation.set(0, -Math.PI/2, 0);
-		hat.scene.position.y = -0.2;
-		hat.scene.position.z = -0.4;
+function animate()
+{
+	requestAnimationFrame(animate);
 
-	}, function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded') ;
-	}, function ( error ) {
-		console.error( error );
-	} );
+	t += timer.getDelta()*5;
+		
+	x=r*(t-Math.sin(t))-20;
+	y=r*(1-Math.cos(t))-0;
+	sphere.position.set(x, y, 0);
+	if(x>20)
+		t=0;	
+
+	camera.position.x = -20+t;
+	camera.position.y = 12-t/2;
+
+	renderer.render( scene, camera );
+}
 
 	renderer.xr.enabled = true;//1
-
-	renderer.setAnimationLoop(() => {
-		renderer.render(scene, camera);
-	});
 
 	const arButton = ARButton.createButton(renderer, {//3a
 		optionalFeatures: ["dom-overlay"],//3b
 		domOverlay: {root: document.body}//3c
 	});//3d
 	document.body.appendChild(arButton);//4
-});
+
+animate();
