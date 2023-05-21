4d104789ef93cdd473070036208c1881c9bbcb4a
DonetskyiIllia <125479114+DonetskyiIllia@users.noreply.github.com>
2023-05-21 11:57:38 +0300

    Update webxr.js

--- webxr.js
+++ webxr.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 	scene.add( group );
 	
 	group.position.set(0, 0, -0.5);
-	group.scale.set(0.005, 0.005, 0.005);
+	group.scale.set(0.01, 0.01, 0.01);
 
 
 	var lightOne=new THREE.AmbientLight(0xffffff, 0.5);
